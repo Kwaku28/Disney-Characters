d58f45b09d73dc556fcda23f3a91f384eedf4a35
Lawrence Amoafo <rekenburg777@gmail.com>
2023-05-24 16:20:40 +0000

    Create function to get info from API
    Add 'extraReducers' that checks the API call if its loading, fulfilled or rejected

---
+++ src/redux/characters/charactersSlice.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-underscore-dangle */
+import {
+  createSlice,
+  createAsyncThunk,
+  isRejectedWithValue,
+} from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  characters: [],
+  status: 'idle',
+  error: null,
+  loading: false,
+};
+
+const URL = 'https://api.disneyapi.dev/character';
+
+export const getCharacters = createAsyncThunk(
+  'characters/getCharacters',
+  async () => {
+    try {
+      const response = await axios.get(URL);
+      return response.data;
+    } catch (error) {
+      return isRejectedWithValue(error.response);
+    }
+  },
+);
+
+const charactersSlice = createSlice({
+  name: 'characters',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCharacters.pending, (state) => {
+        state.status = 'loading';
+        state.loading = true;
+      })
+      .addCase(getCharacters.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.characters = action.payload.data.map((character) => ({
+          id: character._id,
+          name: character.name,
+          characterImage: character.imageUrl,
+        }));
+        state.loading = false;
+      })
+      .addCase(getCharacters.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default charactersSlice.reducer;
