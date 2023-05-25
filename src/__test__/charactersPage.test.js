ce4a8f38f3a6bc0c6d95885c34afad11836bb18e
Lawrence Amoafo <rekenburg777@gmail.com>
2023-05-25 19:17:01 +0000

    Update the tests to pass as expected

--- src/__test__/charactersPage.test.js
+++ src/__test__/charactersPage.test.js
@@ -2,8 +2,18 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
+import { initialState } from '../redux/profile/profileSlice';
 import CharactersPage from '../components/pages/charactersPage';
 import store from '../redux/store';
+
+it('should return the correct initialState', () => {
+  expect(initialState).toEqual({
+    profile: [],
+    status: 'idle',
+    error: null,
+    loading: false,
+  });
+});
 
 it('Renders the exact page', () => {
   const root = renderer.create(
